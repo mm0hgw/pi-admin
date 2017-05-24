0a918085cc48c80b66c4df68ab4392c102dfa4f8
Dale <dale@piratepress.org>
2017-05-24 19:22:59 +0100

    m

---
+++ ldap/ldaptor/R/RFC1123.R
@@ -0,0 +1,36 @@
+
+#RFC1123
+
+RFC1123chars <- c(letters,'-',seq(0,9))
+
+host.class <- function(x){
+    stopifnot(is.host.class(x))
+    x
+}
+
+is.host.class(x){
+    if(!is.character(x))
+        return(FALSE)
+    if(length(x)!=1)
+        return(FALSE)
+    hostchars <- strsplit(x,'')[[1]]
+    if(length(setdiff(hostchars,RFC1123chars))!=0)
+        return(FALSE)
+    return(TRUE)
+}
+
+domain.class <-function(x){
+    stopifnot(is.domain.class(x))
+    x
+}
+
+is.domain.class(x){
+    if(!is.character(x))
+        return(FALSE)
+    if(length(x)!=1)
+        return(FALSE)
+    dcs <- strsplit(x,'\\.')[[1]]
+    if(length(dcs)<2 || !all(sapply(dcs,is.host.class)))
+        return(FALSE)
+    return(TRUE)
+}
