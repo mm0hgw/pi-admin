d20f95ba782c3bea3476a1aeed198c570ccc1e59
Dale <dale@piratepress.org>
2017-05-26 14:39:39 +0100

    buildPackage ldaptor 0.0.0.9001

--- ldap/ldaptor/R/RFC2253.R
+++ ldap/ldaptor/R/RFC2253.R
@@ -78,7 +78,8 @@ format.ldapkv <- function(x, collapse = ": ", ...) {
 #' @param kvlist a 'list' of 'ldapkv' objects (default==0) the content of the query
 #'@export
 ldapquery <- function(pkey, basedn, skeylist = list(), kvlist = list()) {
-	cat(paste('ldapquery:',format(pkey),format(basedn),paste(collapse=':',sapply(skeylist,format)),paste(collapse=':',sapply(kvlist,format))))
+    cat(paste("ldapquery:", format(pkey), format(basedn), paste(collapse = ":", sapply(skeylist, 
+        format)), paste(collapse = ":", sapply(kvlist, format))))
     stopifnot(is.ldapkv(pkey))
     if (valid.domain.class(basedn)) 
         basedn <- basedn.class(basedn)
