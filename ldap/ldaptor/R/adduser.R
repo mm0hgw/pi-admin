440c465d81d11a025aa1fcc6718e8ee26d530578
Dale <dale@piratepress.org>
2017-05-26 23:44:40 +0100

    buildPackage ldaptor 0.0.0.9001

--- ldap/ldaptor/R/adduser.R
+++ ldap/ldaptor/R/adduser.R
@@ -196,31 +196,28 @@ ldapDhcpList <- function(x, key = "dhcpStatements") {
     sapply(x, ldapkv, key = key)
 }
 
-ldapOu <- list(ldapkv("objectClass", "top"),
-    ldapkv("objectClass", "organizationalUnit"))
+ldapOu <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "organizationalUnit"))
 # ou
 
 ldapUser <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "account"), 
     ldapkv("objectClass", "posixAccount"))
-# cn uid uidNumber gidNumber homeDirectory loginShell gecos userPassword: {SASL}uid@REALM
-ldapGroup <- list(ldapkv("objectClass", "top"),
-    ldapkv("objectClass", "posixGroup"))
+# cn uid uidNumber gidNumber homeDirectory loginShell gecos userPassword:
+# {SASL}uid@REALM
+ldapGroup <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "posixGroup"))
 # cn gidNumber
-ldapHost <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "ipHost"), 
-    ldapkv("objectClass", "device"))
+ldapHost <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "ipHost"), ldapkv("objectClass", 
+    "device"))
 # cn ipHostNumber
-ldapNetwork <- list(ldapkv("objectClass", "top"),
-    ldapkv("objectClass", "ipNetwork"))
+ldapNetwork <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "ipNetwork"))
 # cn ipNetworkNumber ipNetmaskNumber
 ldapEther <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "ieee802device"), 
     ldapkv("objectClass", "device"))
 # cn macAddress
-ldapNetgroup <-  list(ldapkv("objectClass", "top"), ldapkv("objectClass", "nisNetgroup"), 
+ldapNetgroup <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "nisNetgroup"), 
     ldapkv("objectClass", "device"))
 # cn nisNetgroupTriple memberNisNetgroup
 
-ldapDhcpServer <- list(ldapkv("objectClass", "top"),  
-    ldapkv("objectClass", "dhcpServer"))
+ldapDhcpServer <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "dhcpServer"))
 # cn dhcpServiceDN
 ldapDhcpSubnet <- list(ldapkv("objectClass", "top"), ldapkv("objectClass", "dhcpSubnet"), 
     ldapkv("objectClass", "dhcpOptions"))
