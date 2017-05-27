528d21f996fb5913398c483279062f8d564f7f0b
Dale <dale@piratepress.org>
2017-05-27 22:09:33 +0100

    buildPackage ldaptor 0.0.0.9001

--- ldap/ldaptor/R/adduser.R
+++ ldap/ldaptor/R/adduser.R
@@ -279,7 +279,7 @@ exportDhcpServers.ldif <- function(realm) {
                 # print(format(skeylist))
                 lq <- ldapquery(pkey, realm$basedn, skeylist, kvlist)
                 # cat(format(lq))
-                hosts <- hosts+ lq
+                hosts <- hosts + lq
                 # print(summary(hosts))
                 
             }
