ebe7e93285c9713e93083db0f70167953c488993
Dale <dale@piratepress.org>
2017-05-31 00:37:55 +0100

    buildPackage ldaptor 0.0.0.9001

--- ldap/ldaptor/R/mac802.R
+++ ldap/ldaptor/R/mac802.R
@@ -108,7 +108,7 @@ mac802list.character <- function(x, ...) {
         
         mac <- mac802(y[1])
         name <- paste(collapse = " ", y[-1])
-        list(mac,name)
+        list(mac, name)
     })
     print(templist)
     out <- lapply(templist, "[[", "mac")
